3007b6b28aeae42572a94b5fe958872f742f80bb
Muhammad Ilham Surya Lesmana <ilhamrampage.mr@gmail.com>
2024-08-14 17:10:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,42 @@ let gold = 50;
 let currentWeapon = 0;
 let fighting;
 let enemyHealth;
-let inventory = ["stick"];
+let inventory = ["hand"];
+
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#hpText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#enemyStats");
+const monsterName = document.querySelector("#enemyrName");
+const monsterHealthText = document.querySelector("#enemyHealth");
+
+const locations = [
+    {
+      name: "town square",
+      "button text": ["Go to store", "Go to cave", "Fight dragon"],
+      "button functions": [goStore, goCave, fightDragon],
+      text: "You are in the town square. You see a sign that says \"Store\"."
+    },
+    {
+      name: "store",
+      "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+      "button functions": [buyHealth, buyWeapon, goTown],
+      text: "You enter the store."
+    },
+    {
+      name: "cave",
+      "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+      "button functions": [fightSlime, fightBeast, goTown],
+      text: "You enter the cave. You see some monsters."
+    },
+    {
+      name: "fight",
+      "button text": ["Attack", "Dodge", "Run"],
+      "button functions": [attack, dodge, goTown],
+      text: "You are fighting a monster."
+    }
+]
